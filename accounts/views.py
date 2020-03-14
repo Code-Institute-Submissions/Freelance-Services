609ee687616395e52535e3ad06bc60ed4fc960bf
ecundick <ecundick@outlook.com>
2020-03-14 17:36:54 +0000

    Added log out confirmation message.

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 # Create your views here.
 
 
 def logout(request):
     """Logs the user out"""
     auth.logout(request)
+    messages.success(request, "You have been succesfully logged out")
     return redirect(reverse('service'))
 
