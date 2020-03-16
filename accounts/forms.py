de5ef7f0ba268cc4cff52e5800fabefb32864a57
ecundick <ecundick@outlook.com>
2020-03-16 07:40:48 +0000

    Added user creation form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class LoginForm(forms.Form):
     """Form for user login"""
@@ -7,7 +9,37 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-class RegisterForm(forms.Form):
+class RegisterForm(UserCreationForm):
     """Form for user login"""
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    
+    password1 = forms.CharField(label="Password",
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Password Confirmation",
+                                widget=forms.PasswordInput)
+ 
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
+
+    #Form error Checking
+    def clean_email(self):
+        """Checks is email is already registered"""
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(email=email).exclude(username=username):
+            raise forms.ValidationError(u'Email already registered')
+        return email
+
+    def clean_password2(self):
+        """Checks passwords match and exist"""
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        
+        if not password1 or not password2:
+            raise ValidationError("Please confirm your password")
+
+        if password1 != password2:
+            raise ValidationError("Passwords must match")
+
+        return password2
