1d07b718543f31b15d499b34b2b74479e031ad89
ecundick <ecundick@outlook.com>
2020-03-23 21:05:15 +0000

    add homepage dict so can be customised from admin

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import HomePage
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    homepage = HomePage.objects.all()
+    return render(request, 'home/index.html', {'homepage': homepage})
