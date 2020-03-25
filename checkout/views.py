c75a118173de2c6b004b7313b6abd05e5093a109
ecundick <ecundick@outlook.com>
2020-03-25 21:00:12 +0000

    Changed logic so cant get to checkout without item

--- checkout/views.py
+++ checkout/views.py
@@ -17,48 +17,51 @@ def checkout(request):
     """
     Used to process payment, 
     """
-    if request.method=='POST':
-        order_form = OrderForm(request.POST) # gets order form from POST
-        payment_form = PaymentForm(request.POST) # gets Payment form from POST
+    if not request.session.get('basket', {}):
+        return redirect(reverse('services'))
+    else:      
+        if request.method=='POST':
+            order_form = OrderForm(request.POST) # gets order form from POST
+            payment_form = PaymentForm(request.POST) # gets Payment form from POST
 
-        if order_form.is_valid() and payment_form.is_valid(): # Checks if forms are valid
-            order = order_form.save(commit=False) 
-            order.date = timezone.now()
-            order.save()
+            if order_form.is_valid() and payment_form.is_valid(): # Checks if forms are valid
+                order = order_form.save(commit=False) 
+                order.date = timezone.now()
+                order.save()
 
 
-            basket = request.session.get('basket', {}) #gets basket contents
-            total = 0 # sets total to 0 for later
-            for id, quantity in basket.items(): # Loops through basket items
-                service = get_object_or_404(Service, pk=id)
-                total += quantity * service.price
-                order_line_item = OrderLineItem(
-                    order = order, 
-                    service = service, 
-                    quantity = quantity
-                )
-                order_line_item.save()
-            try: # trys to make strip payment
-                customer = stripe.Charge.create(
-                    amount = int(total * 100),
-                    currency = "GBP",
-                    description = request.user.email,
-                    card = payment_form.cleaned_data['stripe_id'],
-                )
-            except stripe.error.CardError: # returns error if there is an issue
-                messages.error(request, 'Your card was declined!')
+                basket = request.session.get('basket', {}) #gets basket contents
+                total = 0 # sets total to 0 for later
+                for id, quantity in basket.items(): # Loops through basket items
+                    service = get_object_or_404(Service, pk=id)
+                    total += quantity * service.price
+                    order_line_item = OrderLineItem(
+                        order = order, 
+                        service = service, 
+                        quantity = quantity
+                    )
+                    order_line_item.save()
+                try: # trys to make strip payment
+                    customer = stripe.Charge.create(
+                        amount = int(total * 100),
+                        currency = "GBP",
+                        description = request.user.email,
+                        card = payment_form.cleaned_data['stripe_id'],
+                    )
+                except stripe.error.CardError: # returns error if there is an issue
+                    messages.error(request, 'Your card was declined!')
 
-            if customer.paid: 
-                messages.error(request, 'you have paid')
-                request.session['basket'] = {} # clears basket
-                return redirect(reverse('services'))
+                if customer.paid: 
+                    messages.error(request, 'you have paid')
+                    request.session['basket'] = {} # clears basket
+                    return redirect(reverse('services'))
+                else:
+                    messages.error(request, "Unable to take payment")
             else:
-                messages.error(request, "Unable to take payment")
+                print(payment_form.errors)
+                messages.error(request, "We were unable to take a payment with that card")
         else:
-            print(payment_form.errors)
-            messages.error(request, "We were unable to take a payment with that card")
-    else:
-        payment_form = PaymentForm()
-        order_form = OrderForm()
+            payment_form = PaymentForm()
+            order_form = OrderForm()
 
     return render(request, "checkout/checkout.html", {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})
