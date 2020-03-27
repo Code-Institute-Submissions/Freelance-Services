108bfa4ca16cc3f86bd7e26a5cfe86157208f9c3
ecundick <ecundick@outlook.com>
2020-03-27 20:31:03 +0000

    Added form highlighting

--- static/js/app.js
+++ static/js/app.js
@@ -5,3 +5,19 @@ if($('.alert')){
     }, 3000)
 }
 
+// Checks whether the input field is empty and highlights whether red if empty green if not.
+function checkInput() {
+    var target = $( event.target )
+    if (target.val() === "") {
+        target.css("border", "2px solid red");
+        return false;
+    } else {
+        target.css("border", "2px solid green");
+        return true;
+    }
+}
+
+// Waits for change then calls check input function
+$('input').on("change focusout", () => {
+    checkInput();
+});
